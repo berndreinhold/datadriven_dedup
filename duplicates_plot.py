a1249ef48b80f730d29885d50bf548578b9bb200
berndreinhold <hold-und-rein@web.de>
2022-03-20 12:51:14 +0100

    feat(duplicates_plot.py): documentation for merge_all() and bug fixes. calculate dataset variable and incremental index for plotting instead of user_ids.

--- duplicates_plot.py
+++ duplicates_plot.py
@@ -49,17 +49,29 @@ class duplicates_plot():
                 how="outer", suffixes=(f"_{ds}", None))
 
         # prepare the user_id variables for the merge of OpenAPS_duplicates and OPENonOH_duplicates
-        self.df[f"OpenAPS_duplicates"] = self.df[f"OpenAPS_duplicates"]["date", "user_id", "user_id_OPENonOH"]
+        self.df[f"OpenAPS_duplicates"] = self.df[f"OpenAPS_duplicates"][["date", "user_id", "user_id_OPENonOH"]]
         self.df[f"OpenAPS_duplicates"]["user_id_OpenAPS"] = self.df[f"OpenAPS_duplicates"]["user_id"]
-        self.df[f"OPENonOH_duplicates"] = self.df[f"OPENonOH_duplicates"]["date", "user_id", "user_id_OpenAPS"]  
+        self.df[f"OPENonOH_duplicates"] = self.df[f"OPENonOH_duplicates"][["date", "user_id", "user_id_OpenAPS"]]  
         self.df[f"OPENonOH_duplicates"]["user_id_OPENonOH"] = self.df[f"OPENonOH_duplicates"]["user_id"]
 
         # focus just on the date, user_id_* variables
-        self.df[f"OpenAPS_duplicates"] = self.df[f"OpenAPS_duplicates"]["date", "user_id_OpenAPS", "user_id_OPENonOH"]
-        self.df[f"OPENonOH_duplicates"] = self.df[f"OPENonOH_duplicates"]["date", "user_id_OpenAPS", "user_id_OPENonOH"]  
+        self.df[f"OpenAPS_duplicates"] = self.df[f"OpenAPS_duplicates"][["date", "user_id_OpenAPS", "user_id_OPENonOH"]]
+        self.df[f"OPENonOH_duplicates"] = self.df[f"OPENonOH_duplicates"][["date", "user_id_OpenAPS", "user_id_OPENonOH"]]  
         
-    def merge(self):
-        
+    def merge_all(self):
+        """
+        input: 
+        the merged data frames OpenAPS_duplicates and OPENonOH_duplicates
+
+        output: 
+        df["merged_all"] with entries being uniquely identified by date, user_id_OpenAPS, user_id_OPENonOH
+
+        note:
+        an assumption here is that the sets of user_id_OpenAPS and user_id_OPENonOH are disjunct. Or if they are not disjunct, that they at least refer to the same person.
+        If there are instances where user_id_OpenAPS == user_id_OPENonOH, they will be joined below, even though they might not be in the duplicates list.
+        Probably a not very likely risk: See disjunct_user_ids().
+        """
+
         # then merge these on user_id_OpenAPS, user_id_OPENonOH and date (outer join)
         self.df["merged_all"] = self.df[f"OpenAPS_duplicates"].merge(self.df["OPENonOH_duplicates"], left_on=["date", "user_id_OpenAPS", "user_id_OPENonOH"], 
             right_on=["date", "user_id_OpenAPS", "user_id_OPENonOH"], how="outer")
@@ -74,11 +86,24 @@ class duplicates_plot():
         """
         out = self.df["merged_all"]  # just an alias for easier readibility below
         out.loc[pd.isnull(out["user_id_OPENonOH"]), "dataset"] = 1  # OpenAPS
-        out.loc[~(pd.isnull(out["user_id_OPENonOH"]) | pd.isnull(out["user_id_OpenAPS"])), "dataset"] = 2  # duplicates
         out.loc[pd.isnull(out["user_id_OpenAPS"]), "dataset"] = 3  # OPENonOH
+        out.loc[~(pd.isnull(out["user_id_OPENonOH"]) | pd.isnull(out["user_id_OpenAPS"])), "dataset"] = 2  # duplicates, partially overwrites 1 and 3
         
         out = out.sort_values(by=["dataset", "user_id_OpenAPS", "user_id_OPENonOH", "date"])
 
+        # determine an auto-incremental index "id" for user_id_OpenAPS, user_id_OPENonOH
+        df_user_id = out[["user_id_OpenAPS", "user_id_OPENonOH"]].groupby(["user_id_OpenAPS", "user_id_OPENonOH"], as_index=False).agg("count")    
+        df_user_id = df_user_id.sort_values(["dataset", "user_id_OpenAPS", "user_id_OPENonOH"])
+        df_user_id["id"] = range(len(df_user_id))
+        print(df_user_id)
+
+        # merge the df_user_id with the out dataset
+        self.df["merged_all"] = out.merge(df_user_id, left_on=["user_id_OpenAPS", "user_id_OPENonOH"], right_on=["user_id_OpenAPS", "user_id_OPENonOH"], how="inner")
+        print(self.df["merged_all"])
+        self.df["merged_all"] = self.df["merged_all"].groupby(["date", "id", "dataset"], as_index=False).agg("count")
+        self.df["merged_all"] = self.df["merged_all"][["date", "id", "dataset"]]
+    
+        print(self.df["merged_all"])
 
 
     def plot(self):
@@ -94,7 +119,9 @@ def main():
     #dp.df["duplicates"].info()
 
     dp.merge_with_duplicates_dataset()
-    dp.merge()
+    dp.merge_all()
+    dp.fine_tuning()
+    #dp.plot()
 
 def test():
     dataset = ["OpenAPS", "OPENonOH"]
@@ -110,18 +137,6 @@ def test():
 
 def merge():
     
-    # determine an auto-incremental index for user_id_OpenAPS, user_id_OPENonOH
-    df_user_id = out[["dataset", "user_id_OpenAPS", "user_id_OPENonOH"]].groupby(["dataset", "user_id_OpenAPS", "user_id_OPENonOH"], as_index=False).agg("count")    
-    df_user_id = df_user_id.sort_values(["dataset", "user_id_OpenAPS", "user_id_OPENonOH"])
-    df_user_id["id"] = range(len(df_user_id))
-    print(df_user_id)
-
-    out_OpenAPS = out.merge(df_user_id, left_on=["user_id_OpenAPS", "user_id_OPENonOH"], right_on=["user_id_OpenAPS", "user_id_OPENonOH"], how="inner")
-    print(out_OpenAPS)
-    out_OpenAPS = out_OpenAPS.groupby(["date", "id", "dataset"], as_index=False).agg("count")
-    out_OpenAPS = out_OpenAPS[["date", "id", "dataset"]]
-    
-    print(out_OpenAPS)
 
 	# df3 = df2[["user_id_OpenAPS", "user_id_OPENonOH", "date", "diff_sgv_mean", "diff_sgv_std", "diff_sgv_min", "diff_sgv_max", "diff_sgv_count", "second_id_OPENonOH", "filename_OPENonOH", "filename_OpenAPS"]].sort_values(by=["user_id_OpenAPS", "user_id_OPENonOH", "date"])
 	#df3 = df3[[]]
