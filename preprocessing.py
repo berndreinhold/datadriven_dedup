c67836cab92fbdb1aa6945b62d2b55b70325bb45
berndreinhold <hold-und-rein@web.de>
2022-03-22 23:49:45 +0100

    refact(preprocessing.py): included functions kinds_of_files() and all_entries_json2csv() into class duplicates_preprocessing(). made main_OPENonOH() a member function main() of the class.

--- preprocessing.py
+++ preprocessing.py
@@ -18,17 +18,18 @@ class duplicates_preprocessing():
         self.json_input = IO_json["duplicates_preprocessing"]["input"]
         self.json_output = IO_json["duplicates_preprocessing"]["output"]
 
+        self.column_list = self.json_input["columns"]
+        self.file_ending = self.json_input["file_ending"]
+
         self.error_statistics = {}
         self.key_error_statistics = {}
 
-        self.column_list = self.json_input["columns"]
 
     def load_one_json(self, file_name : str, dir_name : str = ""):
         with open(os.path.join(dir_name, file_name)) as f:
             return json.load(f)
 
 
-# %%
     def one_json2csv(self, entries, dir_name, outfile_name):
         """
         input: this function reads a json-file
@@ -77,25 +78,53 @@ class duplicates_preprocessing():
             if i % 100 == 0: print(i, f)
 
 
+    def kinds_of_files(self, dir_name):
+        """
+        - determine different kinds of files: split filename by "_" and histogram them:
+        - output: {'file_info': 4912, 'devicestatus': 161, 'entries': 161, 'treatments': 161, 'profile': 162}
+        """
+        file_types = {}
+        for i, f in enumerate(glob.glob(os.path.join(f"{dir_name}","**", self.file_ending), recursive=True)):
+            head, tail = os.path.split(f)
+            filename, _ = os.path.splitext(tail)
+            filename_components = filename.split("_")
+            if filename_components[0] == "file":
+                filename_components[0] = filename_components[0] + "_" + filename_components[1]
 
-# %%
-# determine different kinds of files: split filename by "_" and histogram them:
-# output: {'file_info': 4912, 'devicestatus': 161, 'entries': 161, 'treatments': 161, 'profile': 162}
-def kinds_of_files(dir_name, file_ending):
-    file_types = {}
-    for i, f in enumerate(glob.glob(os.path.join(f"{dir_name}","**", file_ending), recursive=True)):
-        head, tail = os.path.split(f)
-        filename, _ = os.path.splitext(tail)
-        filename_components = filename.split("_")
-        if filename_components[0] == "file":
-            filename_components[0] = filename_components[0] + "_" + filename_components[1]
+            if filename_components[0] not in file_types.keys():
+                file_types[filename_components[0]] = 1
+            else:
+                file_types[filename_components[0]] += 1
 
-        if filename_components[0] not in file_types.keys():
-            file_types[filename_components[0]] = 1
-        else:
-            file_types[filename_components[0]] += 1
+        return file_types
 
-    return file_types
+
+
+    # %%
+    def all_entries_json2csv(self, indir_name, outdir_name, column_list):
+        for i, f in enumerate(glob.glob(os.path.join(f"{indir_name}","**", "entries*.json"), recursive=True)):
+            head, tail = os.path.split(f)
+            sub_dirs = head[len(indir_name):]
+            dir_name_components = sub_dirs.split("/")
+            # first = 82868075, second = 21672228 in home/reinhold/Daten/OPEN/OPENonOH_Data/OpenHumansData/82868075/21672228/entries__to_2020-09-11
+            first, second = dir_name_components[0], dir_name_components[1]  
+            filename, _ = os.path.splitext(tail)
+            entries = self.load_one_json(head, tail)
+            # print(outdir_name, os.path.join(first + "_" + second + "_" + filename + ".csv")
+            if len(entries) > 0:
+                self.one_json2csv(entries, outdir_name, first + "_" + second + "_" + filename + ".csv", column_list)
+            else:
+                print(f"{tail} has 0 entries, therefore no output.")
+            del entries
+
+
+    def main(self, dir_name):
+        # %%
+        file_types = self.kinds_of_files(dir_name, "*.json")
+        print(file_types)
+
+        column_list = ["noise", "sgv", "date", "dateString"]
+        self.all_entries_json2csv(dir_name, "/home/reinhold/Daten/OPEN/OPENonOH_Data/csv_per_measurement/", column_list)
 
 
 # %%
@@ -120,23 +149,6 @@ def kinds_of_files_OpenAPS(dir_name, file_ending):
 
     return file_types1, file_types2
 
-
-# %%
-def all_entries_json2csv(indir_name, outdir_name, column_list):
-    for i, f in enumerate(glob.glob(os.path.join(f"{indir_name}","**", "entries*.json"), recursive=True)):
-        head, tail = os.path.split(f)
-        sub_dirs = head[len(indir_name):]
-        dir_name_components = sub_dirs.split("/")
-        # first = 82868075, second = 21672228 in home/reinhold/Daten/OPEN/OPENonOH_Data/OpenHumansData/82868075/21672228/entries__to_2020-09-11
-        first, second = dir_name_components[0], dir_name_components[1]  
-        filename, _ = os.path.splitext(tail)
-        entries = load_one_json(head, tail)
-        # print(outdir_name, os.path.join(first + "_" + second + "_" + filename + ".csv")
-        if len(entries) > 0:
-            one_json2csv(entries, outdir_name, first + "_" + second + "_" + filename + ".csv", column_list)
-        else:
-            print(f"{tail} has 0 entries, therefore no output.")
-        del entries
 
 
 # %%
@@ -176,16 +188,6 @@ def all_entries_json2csv_OpenAPS_part1(indir_name, outdir_name, column_list):
                 error_statistics[e] = 1
 
 # %%
-def main_OPENonOH():
-    # %%
-    dir_name = "/home/reinhold/Daten/OPEN/OPENonOH_Data/OpenHumansData/"
-    file_types = kinds_of_files(dir_name, "*.json")
-    print(file_types)
-
-    column_list = ["noise", "sgv", "date", "dateString"]
-    all_entries_json2csv(dir_name, "/home/reinhold/Daten/OPEN/OPENonOH_Data/csv_per_measurement/", column_list)
-
-# %%
 def main_OpenAPS():
     dir_name = "/home/reinhold/Daten/OPEN/OpenAPS_Data/raw/"
     file_types1, file_types2 = kinds_of_files_OpenAPS(dir_name, "*.json")
@@ -197,6 +199,10 @@ def main_OpenAPS():
 
 
 if __name__ == "__main__":
+    dir_name = "/home/reinhold/Daten/OPEN/OPENonOH_Data/OpenHumansData/"
+    dpp = duplicates_preprocessing()
+    dpp.main(dir_name)
+
     #main_OPENonOH()
     main_OpenAPS()
     print(key_error_statistics)
