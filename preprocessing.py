4eef564194dfcd8bccff674527b01db10c220401
berndreinhold <hold-und-rein@web.de>
2022-03-23 12:37:35 +0100

    feat(preprocessing.py): made class duplicates_preprocessing_OpenAPS_NS with base class duplicates_preprocessing. The two reimplemented functions are all_entries_json2csv() and kinds_of_files(), since the sub-directory is different.

--- preprocessing.py
+++ preprocessing.py
@@ -1,4 +1,5 @@
 # %%
+from distutils.command.config import config
 import json
 import os
 import pandas as pd
@@ -13,17 +14,23 @@ takes a directory containing files with a file_ending specified in the config fi
 """
 
 
-class duplicates_preprocessing():
+class duplicates_preprocessing(object):
+    """
+    process the BG data of OPENonOH
+    """
 
-    def __init__(self, dataset : str, config_filename : str, config_path : str):
+    def __init__(self, config_filename : str, config_path : str, dataset : str = "OPENonOH"):
         """
         read a config file and populate input and output file names and paths:
             - output a csv file with key [user_id, date] and one entry per measurement
         """
         f = open(os.path.join(config_path, config_filename))
         IO_json = json.load(f)
-        self.json_input = IO_json["duplicates_preprocessing"][dataset]["input"]
-        self.json_output = IO_json["duplicates_preprocessing"][dataset]["output"]
+        self.dataset = dataset
+
+        self.json_input = IO_json["duplicates_preprocessing"][self.dataset]["input"]
+        self.json_output = IO_json["duplicates_preprocessing"][self.dataset]["output"]
+
 
         self.in_dir_name = self.json_input["dir_name"]
         self.columns = self.json_input["columns"]
@@ -81,8 +88,12 @@ class duplicates_preprocessing():
 
 
     def all_entries_json2csv(self):
-        for i, f in enumerate(sorted(glob.glob(os.path.join(f"{self.in_dir_name}","**", f"entries*.{self.file_ending}"), recursive=True))):
-            if i < 130: continue
+        file_list = sorted(glob.glob(os.path.join(f"{self.in_dir_name}","**", f"entries*.{self.file_ending}"), recursive=True))
+        print("number of files: ", len(file_list))
+        print(file_list[:3])  # head 
+        print(file_list[-3:])  # and tail
+
+        for i, f in enumerate(file_list):
             if i % 10 == 0: print(i, f)
             head, tail = os.path.split(f)
             sub_dirs = head[len(self.in_dir_name):]
@@ -95,7 +106,6 @@ class duplicates_preprocessing():
 
 
     def extract_json_gz(self, dir_name):
-        # %%
         # for the OpenAPS dataset
         # gunzip json.gz to json files
         # dir_name = "/home/reinhold/Daten/OPEN/OPENonOH_Data/OpenHumansData/00749582/69754"
@@ -139,87 +149,87 @@ class duplicates_preprocessing():
         return file_types
 
 
-    def main(self):
-        # %%
-        self.extract_json_gz(self.in_dir_name)
+    def loop(self):
+        """
+        loop through all available files and do the same thing on each file
+        """
         file_types = self.kinds_of_files(self.in_dir_name)
         print(file_types)
-        #self.all_entries_json2csv()
+        self.all_entries_json2csv()
 
 
-# %%
-# determine different kinds of files: split filename by "_" and histogram them:
-def kinds_of_files_OpenAPS(dir_name, file_ending):
-    file_types1, file_types2 = {},{}
-    for i, f in enumerate(glob.glob(os.path.join(f"{dir_name}","**", file_ending), recursive=True)):
-        head, tail = os.path.split(f)
-        filename, _ = os.path.splitext(tail)
-        filename_components = filename.split("_")
-        if len(filename_components) < 2: 
-            if filename not in file_types1.keys():
-                file_types1[filename] = 1
-            else:
-                file_types1[filename] += 1
-        else: 
-            file_type = filename_components[1]
-            if file_type not in file_types2.keys():
-                file_types2[file_type] = 1
-            else:
-                file_types2[file_type] += 1
-
-    return file_types1, file_types2
-
-
-
-# %%
-def all_entries_json2csv_OpenAPS_part1(indir_name, outdir_name, column_list):
+class duplicates_preprocessing_OpenAPS_NS(duplicates_preprocessing):
     """
-    except AndroidAPS Uploader
+    OpenAPS Nightscout files: direct-sharing-31
     """
-    file_list = sorted(glob.glob(os.path.join(f"{indir_name}","**", "*entries*.json"), recursive=True))
-    print(len(file_list))
-    for i, f in enumerate(file_list):
-        head, tail = os.path.split(f)
-        if i%10==0: print(i, head, tail)
-        filename, _ = os.path.splitext(tail)
-        if os.path.isfile(os.path.join(outdir_name, filename + ".csv")): 
-            # print(f"{os.path.join(outdir_name, filename + '.csv')} exists already")
-            continue
-        sub_dirs = head[len(indir_name):]
-        dir_name_components = sub_dirs.split("/")
-        # first = 82868075, second = 21672228 in home/reinhold/Daten/OPEN/OPENonOH_Data/OpenHumansData/82868075/21672228/entries__to_2020-09-11
-        first, second = dir_name_components[0], dir_name_components[1]  
-        try: 
-            if not second == "direct-sharing-31": raise ValueError(f"not second==direct-sharing-31: {head}, {tail}: {first}, {second}")
-    
-            if first not in filename: raise Exception(f"first not in filename: {head}, {tail}: {first}, {second}")
-            entries = load_one_json(head, tail)
-            # print(outdir_name, os.path.join(first + "_" + second + "_" + filename + ".csv")
-            if len(entries) > 0:
-                one_json2csv(entries, outdir_name, filename + ".csv", column_list)
-            else:
-                print(f"{tail} has 0 entries, therefore no output.")
-            del entries
+    def __init__(self, config_filename : str, config_path : str):
+        self.dataset = "OpenAPS"
+        super().__init__(config_filename, config_path, self.dataset)
+        
 
-        except ValueError as e:
-            if e in error_statistics.keys():
-                error_statistics[e] += 1
-            else:
-                error_statistics[e] = 1
+    def kinds_of_files(self, dir_name):
+        """
+        determine different kinds of files: split filename by "_" and histogram them:
+        """
+        file_types1, file_types2 = {},{}
+        for i, f in enumerate(glob.glob(os.path.join(f"{self.in_dir_name}","**", f"*.{self.file_ending}"), recursive=True)):
+            head, tail = os.path.split(f)
+            filename, _ = os.path.splitext(tail)
+            filename_components = filename.split("_")
+            if len(filename_components) < 2: 
+                if filename not in file_types1.keys():
+                    file_types1[filename] = 1
+                else:
+                    file_types1[filename] += 1
+            else: 
+                file_type = filename_components[1]
+                if file_type not in file_types2.keys():
+                    file_types2[file_type] = 1
+                else:
+                    file_types2[file_type] += 1
 
-# %%
-def main_OpenAPS():
-    dir_name = "/home/reinhold/Daten/OPEN/OpenAPS_Data/raw/"
-    file_types1, file_types2 = kinds_of_files_OpenAPS(dir_name, "*.json")
-    print(file_types1)
-    print(file_types2)
+        return file_types1, file_types2
 
-    all_entries_json2csv_OpenAPS_part1(dir_name, "/home/reinhold/Daten/OPEN/OpenAPS_Data/csv_per_measurement/")
+
+
+    def all_entries_json2csv(self):
+        """
+        focus on NightScout uploads: direct-sharing-31 
+        excluded AndroidAPS Uploader ()
+        """
+        file_list = sorted(glob.glob(os.path.join(f"{self.in_dir_name}","**", "*entries*.json"), recursive=True))
+        print("number of files: ", len(file_list))
+        print(file_list[:3])  # head 
+        print(file_list[-3:])  # and tail
+        for i, f in enumerate(file_list):
+            head, tail = os.path.split(f)
+            if i%10==0: print(i, head, tail)
+            filename, _ = os.path.splitext(tail)
+            if os.path.isfile(os.path.join(self.out_dir_name, filename + ".csv")): 
+                # print(f"{os.path.join(outdir_name, filename + '.csv')} exists already")
+                continue
+            sub_dirs = head[len(self.in_dir_name):]
+            dir_name_components = sub_dirs.split("/")
+            # first = 82868075, second = 21672228 in home/reinhold/Daten/OPEN/OPENonOH_Data/OpenHumansData/82868075/21672228/entries__to_2020-09-11
+            first, second = dir_name_components[0], dir_name_components[1]  
+            try: 
+                if not second == "direct-sharing-31": raise ValueError(f"not second==direct-sharing-31: {head}, {tail}: {first}, {second}")
+        
+                if first not in filename: raise Exception(f"first not in filename: {head}, {tail}: {first}, {second}")
+                self.one_json2csv(head, tail, first + "_" + second + "_" + filename + ".csv")
+                
+            except ValueError as e:
+                if e in self.error_statistics.keys():
+                    self.error_statistics[e] += 1
+                else:
+                    self.error_statistics[e] = 1
 
 
 def main(dataset : str, config_filename : str = "IO.json", config_path : str = "."):
-    dpp = duplicates_preprocessing(dataset, config_filename, config_path)
-    dpp.main()
+    dpp = duplicates_preprocessing(config_filename, config_path)
+    if dataset == "OpenAPS_NS":
+        dpp = duplicates_preprocessing_OpenAPS_NS(config_filename, config_path)
+    dpp.loop()
 
 if __name__ == "__main__":
     fire.Fire(main)
