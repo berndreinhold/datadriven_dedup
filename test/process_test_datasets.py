4031ca7fca576c0fa2f1f2ccc1b9696f4cf9404c
berndreinhold <hold-und-rein@web.de>
2022-04-16 17:08:34 +0200

    feat(process_test_datasets.py): modified the per_day data files, they have not been reduced in size yet.

--- test/process_test_datasets.py
+++ test/process_test_datasets.py
@@ -17,7 +17,7 @@ def config_logger(console_log_level=logging.INFO):
     logger.addHandler(stream_handler)
 
 
-def main():
+def main_duplicates():
     config_logger()
     in_dir_name = ""
     file_list = sorted(glob.glob(os.path.join(f"{in_dir_name}","**", "duplicates*.csv"), recursive=True))
@@ -50,5 +50,35 @@ def modify_one_duplicates_file(filename : str):
     df["filename_ds2"] = df["user_id_ds2"].apply(lambda x: f"{x}_entries_file.csv")
     df.to_csv(filename)
 
+def main_per_day_files():
+    config_logger()
+    in_dir_name = ""
+    file_list = sorted(glob.glob(os.path.join(f"{in_dir_name}","**", "dataset*per_day.csv"), recursive=True))
+    logging.info(f"number of files: {len(file_list)}")
+    #logging.info(file_list[:3])  # head 
+    #logging.info(file_list[-3:])  # and tail
+    for i, f in enumerate(file_list):
+        out = read_one_per_day_file(f)
+        if out[1] > 0:
+            modify_one_per_day_file(f)
+            out = read_one_per_day_file(f)
+        print(out[0], out[1], len(out[2]), len(out[3]), len(out[4]), sorted(out[2]), sorted(out[3]))
+        #print(out[0], out[1], len(out[2]), len(out[3]), len(out[4]))
+
+def read_one_per_day_file(filename : str):
+    df = pd.read_csv(filename, header=0, parse_dates=[1], index_col=0)
+    user_ids = set(df["user_id"].to_numpy().tolist())
+    filenames = set(df["filename"].to_numpy().tolist())
+    user_ids_of_filenames = set(df["filename"].apply(lambda x: x.split('_')[0]).to_numpy().tolist())
+    return (filename, len(df), user_ids, filenames, user_ids_of_filenames)
+
+def modify_one_per_day_file(filename : str):
+    df = pd.read_csv(filename, header=0, parse_dates=[1], index_col=0)
+    df["user_id"] = df["user_id"].astype(float)
+    df["user_id"] = df["user_id"].apply(lambda x: int(x/1000)*1000)
+    df["filename"] = df["user_id"].apply(lambda x: f"{x}_entries_file.csv")
+    df["filename"] = df["user_id"].apply(lambda x: f"{x}_entries_file.csv")
+    df.to_csv(filename)
+
 if __name__ == "__main__":
-    main()
+    main_per_day_files()
