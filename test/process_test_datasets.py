0d7455cfef6caa3eb5ced9bc807d5f16bcf19393
berndreinhold <hold-und-rein@web.de>
2022-04-17 15:08:33 +0200

    feat(process_test_dataset.py): test for two conflict cases. One raises an exception, the other prints a warning.

--- test/process_test_datasets.py
+++ test/process_test_datasets.py
@@ -81,44 +81,65 @@ def modify_one_per_day_file(filename : str):
     df["filename"] = df["user_id"].apply(lambda x: f"{x}_entries_file.csv")
     df.to_csv(filename)
 
-def main():
+def get_the_right_value2(row, col_names : str):
+    x = row[col_names[0]]
+    y = row[col_names[1]]
+
+    # it is clear already from the dataframe selection that one of x or y are not NA
+    if pd.isna(x): return y
+    elif pd.isna(y): return x
+    else:
+        if x == y: return x
+        else: raise ValueError(f"{x} or {y} are not identical, even though they should be, if they are both not NA. row: {row}")
+
+
+def main(conflict_case : int = 1):
     df1 = pd.read_csv("data/merge_test_case.csv", header=0, parse_dates=[1], index_col=None)
-    df2 = pd.read_csv("data/merge_test_case_conflict.csv", header=0, parse_dates=[1], index_col=None)
+    df2 = pd.read_csv(f"data/merge_test_case_conflict{conflict_case}.csv", header=0, parse_dates=[1], index_col=None)
 
-    df_merged = pd.merge(df1, df2, how="outer", left_on="user_id_ds2", right_on="user_id_ds2")
+
+    join_col = "user_id_ds1"  # for merge_test_case_conflict1.csv
+    if conflict_case == 2:
+        join_col = "user_id_ds2"  # for merge_test_case_conflict2.csv
+    df_merged = pd.merge(df1, df2, how="outer", on=join_col)
     
-    def get_the_right_value2(row, col_names : str):
-        x = row[col_names[0]]
-        y = row[col_names[1]]
+    other_cols = set(df1.columns) & set(df2.columns) - set([join_col])
+    print(other_cols)
 
-        # it is clear already from the dataframe selection that one of x or y are not NA
-        if pd.isna(x): return y
-        elif pd.isna(y): return x
-        else:
-            if x == y: return x
-            else: raise ValueError(f"{x} or {y} are not identical, even though they should be, if they are both not NA. row: {row}")
             
 
 
     # process all columns, that the right table brought in (the suffixed columns).
     # give the suffixed colums their old name and merge them 
     
-    #df_merged.loc[(~pd.isna(df_merged["user_id_ds3_x"]) |  ~pd.isna(df_merged["user_id_ds3_y"])), "user_id_ds3"] = df_merged.loc[(~pd.isna(df_merged["user_id_ds3_x"]) |  ~pd.isna(df_merged["user_id_ds3_y"])), ["user_id_ds3_x", "user_id_ds3_y"]].apply(lambda x: get_the_right_value(x[0],x[1]), axis=1)
-    df_merged.loc[(~pd.isna(df_merged["user_id_ds3_x"]) |  ~pd.isna(df_merged["user_id_ds3_y"])), "user_id_ds3"] = df_merged.loc[(~pd.isna(df_merged["user_id_ds3_x"]) |  ~pd.isna(df_merged["user_id_ds3_y"]))].apply(lambda row: get_the_right_value2(row, ["user_id_ds3_x", "user_id_ds3_y"]), axis=1)
+    #col_name = "user_id_ds2"
+    #if conflict_case == 2:
+    #    col_name = "user_id_ds3"
+
+    for col_name in other_cols:
+        per_column(df_merged, col_name)
+
+    # drop the suffixed columns from the merge, as their content has been transferred to the underlying columns:
+    cols = [c for c in df_merged.columns if not c.endswith("_x") and not c.endswith("_y")]
+    df_merged = df_merged[cols]
+
     pdg.show(df_merged)
 
+
+def per_column(df_merged : pd.DataFrame, col_name : str):
+    #df_merged.loc[(~pd.isna(df_merged[f"{col_name}_x"]) |  ~pd.isna(df_merged[f"{col_name}_y"])), col_name] = df_merged.loc[(~pd.isna(df_merged[f"{col_name}_x"]) |  ~pd.isna(df_merged[f"{col_name}_y"])), [f"{col_name}_x", f"{col_name}_y"]].apply(lambda x: get_the_right_value(x[0],x[1]), axis=1)
+    df_merged.loc[(~pd.isna(df_merged[f"{col_name}_x"]) |  ~pd.isna(df_merged[f"{col_name}_y"])), col_name] = df_merged.loc[(~pd.isna(df_merged[f"{col_name}_x"]) |  ~pd.isna(df_merged[f"{col_name}_y"]))].apply(lambda row: get_the_right_value2(row, [f"{col_name}_x", f"{col_name}_y"]), axis=1)
+    #pdg.show(df_merged)
+
     # check for uniqueness
-    if not len(df_merged.loc[~pd.isna(df_merged["user_id_ds3"]), "user_id_ds3"]) == len(df_merged.loc[~pd.isna(df_merged["user_id_ds3"]), "user_id_ds3"].unique()):
-        count_all = len(df_merged.loc[~pd.isna(df_merged["user_id_ds3"]), "user_id_ds3"])
-        count_unique = len(df_merged.loc[~pd.isna(df_merged["user_id_ds3"]), "user_id_ds3"].unique())
+    if not len(df_merged.loc[~pd.isna(df_merged[col_name]), col_name]) == len(df_merged.loc[~pd.isna(df_merged[col_name]), col_name].unique()):
+        count_all = len(df_merged.loc[~pd.isna(df_merged[col_name]), col_name])
+        count_unique = len(df_merged.loc[~pd.isna(df_merged[col_name]), col_name].unique())
         #raise ValueError(f"values are not unique anymore in column 'user_id_ds3': count(all): {count_all}, count(unique): {count_unique}")
         print(f"values are not unique anymore in column 'user_id_ds3': count(all): {count_all}, count(unique): {count_unique}")
 
-    # drop the columns from the merge:
-    df_merged = df_merged[["user_id_ds1_x", "user_id_ds1_y", "user_id_ds2", "user_id_ds3"]]
-
-    pdg.show(df_merged)
 
 if __name__ == "__main__":
     #main_per_day_files()
-    main()
+    #main(1)
+    main(2)
