ea3dc1e9fcb79e8366c6f738480c1c5e826d51a0
berndreinhold <hold-und-rein@web.de>
2022-05-14 14:05:37 +0200

    feat(generaet_artificial_data.py): moveed the simulation of the daily stats into a separate function sim_PG_daily_stats(). Fixed the loop() to copy (1,2) to (2,1) rather than calling create_one_dataset(2,1), which is 0 in the matrices in the config file.

--- generate_artificial_data.py
+++ generate_artificial_data.py
@@ -63,8 +63,23 @@ class artificial_datasets():
         pm_ids2 = self.random.integers(10000000,99999999, count_project_member_ids)
         return pm_ids1, pm_ids2  # random 8 bit number
 
-    def sim_PG_one_day() -> tuple:  # PG: plasma glucose
-        pass
+    def sim_PG_daily_stats(self, count_days) -> tuple:  
+        # simulate plasma glucose (PG) statistical variables
+        # simulate for all days (count_days), 
+        # do not take into account information from actual data, correlations, etc.
+        # Possible future improvement:have an individual mean and std per project_member_id
+        
+        PG_mean = self.random.normal(120, 20, count_days)
+        PG_std = self.random.normal(20, 15, count_days)
+        PG_min, PG_max, PG_count = [], [], []
+        min_factor = self.random.normal(3, 1, count_days)
+        max_factor = self.random.normal(4, 1, count_days)
+        for k in range(count_days):
+            PG_min.append(PG_mean[k] - min_factor[k]*PG_std[k])
+            PG_max.append(PG_mean[k] + max_factor[k]*PG_std[k])
+            PG_count.append(288) # 288: 24h*measurements every 5 min
+
+        return PG_mean, PG_std, PG_min, PG_max, PG_count
 
 
     def validate_config_file(self):
@@ -78,6 +93,7 @@ class artificial_datasets():
         """
         count_project_member_ids = self.matrix_project_member_id[i][j]
         count_days = self.matrix_day[i][j]
+        if count_days < 1: print(f"Warning: count_days was < 1 ({count_days}) for (i: {i},j: {j})")
         pm_ids1, pm_ids2 = self.project_member_ids(i,j)  # len(pm_ids1) == count_project_member_ids
         pm_id_indices1 = self.random.integers(0, count_project_member_ids, count_days)
         pm_id_indices2 = self.random.integers(0, count_project_member_ids, count_days)  # only necessary if not i==j
@@ -85,14 +101,7 @@ class artificial_datasets():
         pm_id_indices1 = sorted(pm_id_indices1)
         pm_id_indices2 = sorted(pm_id_indices2)  # only necessary if not i==j, but simulated anyway
 
-        PG_mean = self.random.normal(120, 20, count_days)
-        PG_std = self.random.normal(20, 15, count_days)
-        PG_min, PG_max = [],[]
-        min_factor = self.random.normal(3, 1, count_days)
-        max_factor = self.random.normal(4, 1, count_days)
-        for k in range(count_days):
-            PG_min.append(PG_mean[k] - min_factor[k]*PG_std[k])
-            PG_max.append(PG_mean[k] + max_factor[k]*PG_std[k])
+        PG_mean, PG_std, PG_min, PG_max, PG_count = self.sim_PG_daily_stats(count_days)
 
         # date,sgv_mean,sgv_std,sgv_min,sgv_max,sgv_count,filename,user_id
         data = []
@@ -101,27 +110,30 @@ class artificial_datasets():
             if i==j:
                 project_member_id = pm_ids1[pm_id_indices1[k]]
                 # mean, std, min, max, count, filename, project_member_id
-                data.append(["2021-05-35", PG_mean[k], PG_std[k], PG_min[k], PG_max[k], 288, f"test_{i}_{j}.csv", project_member_id])  # 288: 24h*measurements every 5 min
+                data.append(["2021-05-35", PG_mean[k], PG_std[k], PG_min[k], PG_max[k], PG_count[k], f"test_{i}_{j}.csv", project_member_id])  
             else:
-                data.append(["2021-05-35", PG_mean[k], PG_std[k], PG_min[k], PG_max[k], 288, f"test_{i}_{j}.csv", pm_ids1[pm_id_indices1[k]], pm_ids2[pm_id_indices2[k]]])
+                data.append(["2021-05-35", PG_mean[k], PG_std[k], PG_min[k], PG_max[k], PG_count[k], f"test_{i}_{j}.csv", pm_ids1[pm_id_indices1[k]], pm_ids2[pm_id_indices2[k]]])
         return data
 
     def loop(self):
         """
         loop through all datasets
+        simulate all datasets in the matrix (i, j). Only the datasets for i>=j need to be simulated using create_one_dataset(i,j). (2,1) is the same as (1,2), therefore it can be copied.
         """
         # create days for each combination of (i,j) 
         cols = ["date", "sgv_mean", "sgv_std", "sgv_min", "sgv_max", "sgv_count", "filename"]
         for i in range(self.count_datasets):
-            self.days.append([])
-            self.df_days.append([])
+            self.days.append([])  # data as a list of rows
+            self.df_days.append([])  # the corresponding dataframe
             for j in range(self.count_datasets):
-                self.days[i].append(self.create_one_dataset(i,j))
+                if j >= i: day_data = self.create_one_dataset(i,j)
+                else: day_data = self.days[j][i]
+                self.days[i].append(day_data)
+
+                cols2 = deepcopy(cols)
                 if i==j:
-                    cols2 = deepcopy(cols)
                     cols2.append(f"project_member_id_{i}")
                 else:
-                    cols2 = deepcopy(cols)
                     cols2.append(f"project_member_id_{i}")
                     cols2.append(f"project_member_id_{j}")
                 df = pd.DataFrame(data=self.days[i][j], columns=cols2)
