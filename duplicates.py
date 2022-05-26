a656bee16e31ff95cf65007b0bd82d7f04b66f35
berndreinhold <hold-und-rein@web.de>
2022-05-27 00:22:08 +0200

    feat(duplicates.py,config_master_sim.json): config_master_sim.json in steps the group is called "preparation" rather than "individual dataset" since as of right now the generate_artificial_data.py produces the output of the "individual_dataset" steps. No functional changes in duplicates.py, but the config_pairwise.json, which is read as config file has a new, simplified structure. Also user_id was renamed to project_member_id.

--- duplicates.py
+++ duplicates.py
@@ -12,53 +12,66 @@ class duplicates_pairwise():
     def __init__(self, config_filename : str, config_path : str):
         """
         read a config file and populate file names and paths of three csv files:
-            - OpenAPS with key [user_id, date]
-            - OPENonOH with key [user_id, date]
+            - OpenAPS with key [project_member_id, date]
+            - OPENonOH with key [project_member_id, date]
         output:
-            - duplicates file containing the duplicates between the two data files with key [user_id_ds1, user_id_ds2, date]
+            - duplicates file containing the duplicates between the two data files with key [project_member_id_ds1, project_member_id_ds2, date]
         """
         f = open(os.path.join(config_path, config_filename))
         IO_json = json.load(f)
         self.IO = IO_json["duplicates_pairwise"]["IO"]
-        self.diff_svg_threshold = IO_json["duplicates_pairwise"]["diff_svg_threshold"]
+        self.diff_svg_threshold = float(IO_json["duplicates_pairwise"]["diff_svg_threshold"][0])
+        self.root_data_dir_name = IO_json["root_data_dir_name"]
+
 
     def init_one_pair(self, i : int):
         self.json_input = self.IO[i]["input"]
         self.json_output = self.IO[i]["output"]
 
-        self.datasets = [x for x in self.json_input.keys() if "comment" not in x]
-        self.df = {}
-        for ds in self.datasets:
-            infile = self.json_input[ds]
-            self.df[ds] = pd.read_csv(os.path.join(infile[0], infile[1]), header=0, parse_dates=[1], index_col=0)
-            self.df[ds]["date"] = pd.to_datetime(self.df[ds]["date"],format="%Y-%m-%d")
+        self.df = []
+        infile = self.json_input[0]
+        self.df.append(pd.read_csv(os.path.join(self.root_data_dir_name, infile[0], infile[1]), header=0, parse_dates=[1], index_col=0))
+        self.df[0]["date"] = pd.to_datetime(self.df[0]["date"],format="%Y-%m-%d")
 
+        infile = self.json_input[1]
+        self.df.append(pd.read_csv(os.path.join(self.root_data_dir_name, infile[0], infile[1]), header=0, parse_dates=[1], index_col=0))
+        self.df[1]["date"] = pd.to_datetime(self.df[1]["date"],format="%Y-%m-%d")
 
     def create_duplicates_file(self, i : int, debug = False) -> pd.DataFrame:
-        id0 = self.datasets[0]
-        id1 = self.datasets[1]
-        out = self.df[id0].merge(self.df[id1], left_on="date", right_on="date", how="inner", suffixes=("_ds1", "_ds2"))
+        # load correct IO variables
+        self.json_input = self.IO[i]["input"]
+        self.json_output = self.IO[i]["output"]
+        id = ["_" + self.json_input[0][2], "_" + self.json_input[1][2]]
 
-        for col in self.df[id0].columns:
-            if not "sgv" in col: continue
-            out["diff_" + col] = 2*(out[col + "_ds1"] - out[col + "_ds2"])/(out[col + "_ds1"] + out[col + "_ds2"])
+        out = self.df[0].merge(self.df[1], left_on="date", right_on="date", how="inner", suffixes=(id[0], id[1]))
+
+        for col in self.df[0].columns:
+            if not "sgv" in col: continue  # only columns containing "sgv": sgv: single glucose value (?)
+            out["diff_" + col] = 2*(out[col + id[0]] - out[col + id[1]])/(out[col + id[0]] + out[col + id[1]])
 
         df2 = out[(out.diff_sgv_mean*out.diff_sgv_mean < self.diff_svg_threshold) & (out.diff_sgv_std*out.diff_sgv_std < self.diff_svg_threshold) & 
             (out.diff_sgv_min*out.diff_sgv_min < self.diff_svg_threshold) & (out.diff_sgv_max*out.diff_sgv_max < self.diff_svg_threshold) & 
             (out.diff_sgv_count*out.diff_sgv_count < self.diff_svg_threshold)]
-        df3 = df2[["user_id_ds1", "user_id_ds2", "date", "diff_sgv_mean", "diff_sgv_std", "diff_sgv_min", "diff_sgv_max", "diff_sgv_count", 
-            "filename_ds1", "filename_ds2"]].sort_values(by=["user_id_ds1", "user_id_ds2", "date"])
+        df3 = df2[[f"project_member_id{id[0]}", f"project_member_id{id[1]}", "date", "diff_sgv_mean", "diff_sgv_std", "diff_sgv_min", "diff_sgv_max", "diff_sgv_count", 
+            f"filename{id[0]}", f"filename{id[1]}"]].sort_values(by=[f"project_member_id{id[0]}", f"project_member_id{id[1]}", "date"])
 
         if debug and len(df3)>0: gui = pdg.show(df3)
 
-        outdir = self.json_output["duplicates"][0]
-        outfilename = self.json_output["duplicates"][1]
-        df3.to_csv(os.path.join(outdir, outfilename))
-        print(os.path.join(outdir, outfilename) + " created")
+        df3.to_csv(os.path.join(self.root_data_dir_name, self.json_output[0], self.json_output[1]))
+        print(os.path.join(self.root_data_dir_name, self.json_output[0], self.json_output[1]) + " created")
         return df3
 
-    def validation(self, df : pd.DataFrame):
-        # group by statement has to return exactly one line, otherwise the selection criteria on diff_sgv_*-variables need to be tightened.
+    def validation(self, i : int, df : pd.DataFrame):
+        """
+        group by statement involving all diff_sgv-variables has to return exactly one line, 
+        otherwise the selection criteria on diff_sgv_*-variables (the diff_sgv_threshold) need to be tightened.
+        """
+
+        # load correct IO variables
+        self.json_input = self.IO[i]["input"]
+        self.json_output = self.IO[i]["output"]
+        id = ["_" + self.json_input[0][2], "_" + self.json_input[1][2]]
+
 
         df_duplicates_only = df.groupby(["diff_sgv_mean", "diff_sgv_std", "diff_sgv_min", "diff_sgv_max", "diff_sgv_count"]).agg(["count"])
         df2 = df_duplicates_only  # just a bit more compact 
@@ -79,9 +92,10 @@ class duplicates_pairwise():
                     print(df2[col]*df2[col] > self.diff_svg_threshold)
                     raise ValueError(f"{col}^2 should be smaller than diff_svg_threshold ({self.diff_svg_threshold})")
 
+
         # get list of duplicates and the count of days
-        df3_mod = df[['user_id_ds1', 'user_id_ds2','date']]
-        df4 = df3_mod.groupby(['user_id_ds1', 'user_id_ds2']).agg(["count"])
+        df3_mod = df[[f"project_member_id{id[0]}", f"project_member_id{id[1]}",'date']]
+        df4 = df3_mod.groupby([f"project_member_id{id[0]}", f"project_member_id{id[1]}"]).agg(["count"])
 
         columns = []
         for col in df4.columns:
@@ -89,7 +103,7 @@ class duplicates_pairwise():
         df4.columns = columns
         df4.fillna(value=0, inplace=True)  #
         df4.reset_index(inplace=True)
-        #print(df4[['user_id_ds1', 'user_id_ds2','date_count']])
+        #print(df4[[f"project_member_id{id[0]}", f"project_member_id{id[1]}",'date_count']])
         print(df4)
 
 
@@ -98,11 +112,11 @@ class duplicates_pairwise():
             print(one_pair)
             self.init_one_pair(i)
             df = self.create_duplicates_file(i)
-            if len(df) > 0: self.validation(df)
+            if len(df) > 0: self.validation(i, df)
 
 
 
-def main(config_filename : str = "IO.json", config_path : str = "."):
+def main(config_filename : str = "config_pairwise.json", config_path : str = "."):
 
     dp = duplicates_pairwise(config_filename, config_path)
     dp.loop()
